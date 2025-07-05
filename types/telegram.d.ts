09feb338b2e5b0b97a16283f6cb63cecdef4e5ce
Ayush Pratap <pratapayush4849@gmail.com>
2025-07-05 20:45:25 +0000

    mini app appbar fixes

---
+++ types/telegram.d.ts
@@ -0,0 +1,42 @@
+interface TelegramWebAppUser {
+  id: number;
+  first_name: string;
+  last_name?: string;
+  username?: string;
+  language_code?: string;
+}
+
+interface TelegramWebApp {
+  initData: string;
+  initDataUnsafe: {
+    user?: TelegramWebAppUser;
+    [key: string]: any;
+  };
+  colorScheme: 'light' | 'dark';
+  themeParams: {
+    [key: string]: string;
+  };
+  isExpanded: boolean;
+  expand(): void;
+  close(): void;
+  BackButton: {
+    show(): void;
+    hide(): void;
+    onClick(callback: () => void): void;
+  };
+  MainButton: {
+    show(): void;
+    hide(): void;
+    onClick(callback: () => void): void;
+    setText(text: string): void;
+  };
+  ready(): void;
+}
+
+interface TelegramWindow extends Window {
+  Telegram?: {
+    WebApp: TelegramWebApp;
+  };
+}
+
+declare const window: TelegramWindow;
